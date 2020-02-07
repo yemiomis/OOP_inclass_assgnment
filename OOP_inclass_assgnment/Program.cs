a65501af25b27b758a9f584b87a6e5fbf4b3c6bf
Adeyemi Omisakin <aomisakin@usf.edu>
2020-02-07 11:59:33 -0500

    assign.

---
+++ OOP_inclass_assgnment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP_inclass_assgnment
+{
+    class assignment
+    {
+        string student_name = "vini";
+        string prof_name = "shaolin";
+        public int no_of_student = 45;
+        static void Main(string[] args)
+        {
+            assignment first_assgn = new assignment();
+            assignment second_assgn = new assignment();
+            Console.WriteLine(first_assgn.student_name);
+            Console.WriteLine(second_assgn.prof_name);
+        }
+            class people
+        {
+
+             }
+    }
+}
+
